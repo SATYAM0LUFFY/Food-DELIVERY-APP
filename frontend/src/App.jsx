d37acab34c5c9a1a75672be81863588bf0fe89ed
SATYAM SINGH <satyamsinghu123123@gmail.com>
2024-11-01 18:06:38 +0530

    Update App.jsx correcter navbar to Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar/navbar'
+import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from "./pages/Home/Home"
 import Cart from "./pages/Cart/Cart"
